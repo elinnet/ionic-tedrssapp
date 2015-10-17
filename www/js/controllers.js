388101452caceb823ae0f93740aed5d96e560726
Elin <elinnet@gmail.com>
2015-10-17 11:55:53 +0100

    creation of post page from feed - updated

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,10 +20,13 @@ app.controller('FeedCtrl', function ($scope, $ionicLoading, FeedService) {
 
 app.controller('PostCtrl', function ($scope, $stateParams, FeedService) {
 	console.log("Loading PostCtrl");
-	console.log($stateParams);
+	console.log($stateParams); //this will get the url of what you click through
 	$scope.postId = $stateParams.id;
-	$scope.post = FeedService.getEntry($scope.postId);
+	$scope.post = FeedService.getEntry($scope.postId); //this will match your url with the data pulled through from tedx rss, get the full file and then have this ready for the post.html page to present
 	console.log($scope.post);
+
+//
+
 
 	$scope.share = function () {
 		console.debug("Sharing post");
